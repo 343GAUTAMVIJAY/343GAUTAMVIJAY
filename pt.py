564081f0cc8160d9444734bbc9f658d9407d2765
343GAUTAMVIJAY <147785180+343GAUTAMVIJAY@users.noreply.github.com>
2024-12-28 10:44:40 +0530

    Create pt.py

---
+++ pt.py
@@ -0,0 +1,17 @@
+# a=int(input("Enter a: "))
+# b=int(input("Enter b: "))
+# print(f"The sum is {a*b}") if (a*b)<=1000 else print(f"The product is {a+b}")
+
+# for i in range(0,10):
+#     print(f"Currrent number {i} Previous number {i-1} Sum: {i+(i-1)}")
+
+# s=input("ENter ur string: ")
+# for i in range(0,len(s),2):
+#     print(s[i])
+    
+# lst=['a','e','i','o','u']
+# l=input("Enter letter: ")
+# if l in lst:
+#     print("Yep it's vowel")
+# else:
+#     print("Nope it's try another letter")
